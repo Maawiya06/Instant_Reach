0149f68dcd205ddd8b933d28375235f18c8db609
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-11 01:16:08 +0530

    commit

---
+++ src/main/java/com/InstanceReach/InstanceReachApplication.java
@@ -0,0 +1,13 @@
+package com.InstanceReach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanceReachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstanceReachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InstanceReach/InstanceReachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InstanceReach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstanceReachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
