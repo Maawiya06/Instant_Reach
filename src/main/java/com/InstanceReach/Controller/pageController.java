4d5d345603ad17de3ad39d7064e2bb5cc837839d
Maawiya06 <maawiyaameer001@gmail.com>
2026-03-07 00:02:26 +0530

    commit

--- src/main/java/com/InstanceReach/Controller/pageController.java
+++ src/main/java/com/InstanceReach/Controller/pageController.java
@@ -1,6 +1,7 @@
 package com.InstanceReach.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,16 @@ public class pageController {
     @RequestMapping("/home")
     public String home(){
         return "home";
+    }
+
+    // about
+    @RequestMapping("/about")
+    public String About(Model m){
+        return "about";
+    }
+    // services
+    @RequestMapping("/services")
+    public String Services(Model m){
+        return "services";
     }
 }
